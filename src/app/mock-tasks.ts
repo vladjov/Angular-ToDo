87648a5ed31bd34b7600d65aa391f5470c4afb48
Vladimir Jovanovic <vladimir.jovanovic.biz@gmail.com>
2021-11-13 13:37:48 +0100

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Fix the car",
+        day: "May 5th at 14:30",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "School meeting",
+        day: "May 7th at 17:30",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Grocery shopping",
+        day: "May 9th at 19:00",
+        reminder: false,
+    }
+]
